e57854b496c396736e39ceae1959af8a04ae2b88
William Bishop <bishopwm@g.cofc.edu>
2015-07-23 12:30:55 +0200

    fixed migrations issue

--- db/migrate/20150708201405_create_products.rb
+++ db/migrate/20150708201405_create_products.rb
@@ -1,12 +1,12 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
-    	t.integer :user_id
+    	    	t.integer :user_id
     	t.string :title
     	t.text :description
     	t.date :deadline
+
+    	t.timestamps null: false
     end
   end
 end
-
